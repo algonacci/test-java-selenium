2ef0fa30a90aef80a319983a26702f6f4e38da39
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2023-09-25 21:54:25 +0700

    add simple actions

--- src/main/java/org/braincore/Main.java
+++ src/main/java/org/braincore/Main.java
@@ -1,7 +1,24 @@
 package org.braincore;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://braincore.id");
+        try {
+            Thread.sleep(3000);
+            WebElement element = driver.findElement(By.linkText("Mulai Cari Tahu"));
+            element.click();
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            driver.quit();
+        }
     }
 }
